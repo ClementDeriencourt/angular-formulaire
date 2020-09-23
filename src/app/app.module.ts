e52a8f6f217bb932fa308b5d55b015cb7e1843f4
Clément Deriencourt <clement-deriencourt@orange.fr>
2020-09-23 14:20:19 +0200

    Formulaire avec tests boutons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroFormComponent } from './hero-form/hero-form.component';
+import { BoutonTestComponent } from './bouton-test/bouton-test.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+
+    MatButtonModule,
+    MatIconModule,
+
+
+
+
   ],
   declarations: [
     AppComponent,
-    HeroFormComponent
+    HeroFormComponent,
+    BoutonTestComponent,
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA,
   ],
   providers: [],
   bootstrap: [ AppComponent ]
---
+++ src/app/bouton-test/bouton-test.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+
+
+
+
+@Component({
+  selector: 'app-bouton-test',
+  templateUrl: './bouton-test.component.html',
+  styleUrls: ['./bouton-test.component.css']
+})
+export class BoutonTestComponent {}
+
